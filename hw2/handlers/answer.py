6a971ba336f2ae5e5eb86e518aa51cb17405bb6e
Xasigaki <nursi.07060504@gmail.com>
2023-02-12 21:03:46 +0000

    hw1

--- hw2/handlers/answer.py
+++
@@ -1,9 +0,0 @@
-from aiogram import types
-from handlers import constans
-
-
-async def qwerty(message: types.Message):
-    '''
-    Функция ответа пользователю
-    '''
-    await message.answer(text=constans.ANSWER_BOT)
--- hw2/handlers/constans.py
+++
@@ -1,8 +0,0 @@
-START_BOT = '''
-WELCOME TO ICHIRAKU RAMEN!
-'''
-
-ANSWER_BOT = '''
-welcome to ichiraku ramen
- click to get started /start
-'''
